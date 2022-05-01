358c3a7ddb68d7603a13299c158f2d2ba3fed9a8
Neil Ryan L <neilryanlipaod@gmail.com>
2022-05-01 12:24:32 +0800

    Modify createSnippet to reject non POST requests

--- main.go
+++ main.go
@@ -6,6 +6,10 @@ import (
 )
 
 func home(w http.ResponseWriter, r *http.Request) {
+	if r.URL.Path != "/" {
+		http.NotFound(w, r)
+		return
+	}
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
@@ -14,6 +18,13 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 }
 
 func createSnippet(w http.ResponseWriter, r *http.Request) {
+
+	if r.Method != http.MethodPost {
+		w.WriteHeader(405)
+		w.Write(([]byte("Method Not Allowed")))
+		return
+	}
+
 	w.Write([]byte("Create a new snippet..."))
 }
 
