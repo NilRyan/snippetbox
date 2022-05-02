f3a01837c5ac5f594c253e2ceab15c106bd4874c
Neil Ryan L <neilryanlipaod@gmail.com>
2022-05-02 20:05:17 +0800

    Utilize http.Error for error handling

--- main.go
+++ main.go
@@ -20,8 +20,8 @@ func showSnippet(w http.ResponseWriter, r *http.Request) {
 func createSnippet(w http.ResponseWriter, r *http.Request) {
 
 	if r.Method != http.MethodPost {
-		w.WriteHeader(405)
-		w.Write(([]byte("Method Not Allowed")))
+		w.Header().Set("Allow", http.MethodPost)
+		http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
 		return
 	}
 
