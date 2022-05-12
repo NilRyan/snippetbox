4f08722fa9e6abdda9740198535ea7a1cdf48ade
Neil Ryan L <neilryanlipaod@gmail.com>
2022-05-12 20:15:06 +0800

    chore: add addr flag for cli params

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,11 +1,14 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
 )
 
 func main() {
+	addr := flag.String("addr", ":4000", "HTTP network address")
+	flag.Parse()
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -15,7 +18,7 @@ func main() {
 
 	mux.Handle("/static/", http.StripPrefix(("/static"), fileServer))
 
-	log.Println("Starting server on :4000")
-	err := http.ListenAndServe(":4000", mux)
+	log.Printf("Starting server on %s", *addr)
+	err := http.ListenAndServe(*addr, mux)
 	log.Fatal(err)
 }
