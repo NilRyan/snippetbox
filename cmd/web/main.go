23f1e5ed7051095129efe7a7eaea0cabad1ed78c
Neil Ryan L <neilryanlipaod@gmail.com>
2022-05-12 22:07:42 +0800

    chore(main): add leveled logging

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 type Config struct {
@@ -16,6 +17,10 @@ func main() {
 	flag.StringVar(&config.Addr, "addr", ":4000", "HTTP network address")
 	flag.StringVar(&config.StaticDir, "static-dir", "./ui/static", "Path to static assets")
 	flag.Parse()
+
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
 	mux.HandleFunc("/snippet", showSnippet)
@@ -25,7 +30,7 @@ func main() {
 
 	mux.Handle("/static/", http.StripPrefix(("/static"), fileServer))
 
-	log.Printf("Starting server on %s", config.Addr)
+	infoLog.Printf("Starting server on %s", config.Addr)
 	err := http.ListenAndServe(config.Addr, mux)
-	log.Fatal(err)
+	errorLog.Fatal(err)
 }
