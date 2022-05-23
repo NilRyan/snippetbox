ffcd2d9c78799d75eeb02cba7fc6e4920b765a6f
Neil Ryan L <neilryanlipaod@gmail.com>
2022-05-23 19:42:16 +0800

    refactor(routes): add routes.go to encapsulate routes in its own file

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -30,19 +30,10 @@ func main() {
 		infoLog:  infoLog,
 	}
 
-	mux := http.NewServeMux()
-	mux.HandleFunc("/", app.home)
-	mux.HandleFunc("/snippet", app.showSnippet)
-	mux.HandleFunc("/snippet/create", app.createSnippet)
-
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-
-	mux.Handle("/static/", http.StripPrefix(("/static"), fileServer))
-
 	srv := &http.Server{
 		Addr:     config.Addr,
 		ErrorLog: errorLog,
-		Handler:  mux,
+		Handler:  app.routes(),
 	}
 
 	infoLog.Printf("Starting server on %s", config.Addr)
