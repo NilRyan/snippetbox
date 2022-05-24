6e6027e2588a9fd0f7b480ad87e62afc5ee59984
Neil Ryan L <neilryanlipaod@gmail.com>
2022-05-24 22:30:58 +0800

    feat(models): add insert function to SnippetModel

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -11,7 +11,22 @@ type SnippetModel struct {
 }
 
 func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
-	return 0, nil
+	stmt := `INSERT INTO snippets (title, content, created, expires)
+	VALUES(?, ?, UTC_TIMESTAMP(), DATE_ADD(UTC_TIMESTAMP(), INTERVAL ? DAY))`
+
+	result, err := m.DB.Exec(stmt, title, content, expires)
+	if err != nil {
+
+		return 0, nil
+	}
+
+	id, err := result.LastInsertId()
+
+	if err != nil {
+		return 0, nil
+	}
+
+	return int(id), nil
 }
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
