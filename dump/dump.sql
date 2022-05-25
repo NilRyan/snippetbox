3f2faaa1ceae2594bcc8f06e3c975c18688695ab
Neil Ryan L <neilryanlipaod@gmail.com>
2022-05-25 21:50:05 +0800

    feat(models): initialize db and add insert snippet in handler

---
+++ dump/dump.sql
@@ -0,0 +1,26 @@
+-- Create a `snippets` table.
+CREATE TABLE snippets (
+id INTEGER NOT NULL PRIMARY KEY AUTO_INCREMENT, title VARCHAR(100) NOT NULL,
+content TEXT NOT NULL,
+created DATETIME NOT NULL,
+expires DATETIME NOT NULL
+);
+-- Add an index on the created column.
+CREATE INDEX idx_snippets_created ON snippets(created);
+
+-- Add some dummy records (which we'll use in the next couple of chapters).
+INSERT INTO snippets (title, content, created, expires) VALUES (
+'An old silent pond',
+'An old silent pond...\nA frog jumps into the pond,\nsplash! Silence again.\n\n– Matsuo Bashō', UTC_TIMESTAMP(),
+DATE_ADD(UTC_TIMESTAMP(), INTERVAL 365 DAY)
+);
+INSERT INTO snippets (title, content, created, expires) VALUES (
+'Over the wintry forest',
+'Over the wintry\nforest, winds howl in rage\nwith no leaves to blow.\n\n– Natsume Soseki', UTC_TIMESTAMP(),
+DATE_ADD(UTC_TIMESTAMP(), INTERVAL 365 DAY)
+);
+INSERT INTO snippets (title, content, created, expires) VALUES (
+'First autumn morning',
+'First autumn morning\nthe mirror I stare into\nshows my father''s face.\n\n– Murakami Kijo', UTC_TIMESTAMP(),
+DATE_ADD(UTC_TIMESTAMP(), INTERVAL 7 DAY)
+);
