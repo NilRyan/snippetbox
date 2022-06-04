5bd4f4a1c882e1859465758d0dd9536d79cbcf5b
Neil Ryan L <neilryanlipaod@gmail.com>
2022-06-04 23:58:26 +0800

    feat(models): add get function for snippet

--- pkg/models/mysql/snippets.go
+++ pkg/models/mysql/snippets.go
@@ -2,6 +2,7 @@ package mysql
 
 import (
 	"database/sql"
+	"errors"
 
 	"indifferentcat.dev/snippetbox/pkg/models"
 )
@@ -30,8 +31,22 @@ func (m *SnippetModel) Insert(title, content, expires string) (int, error) {
 }
 
 func (m *SnippetModel) Get(id int) (*models.Snippet, error) {
+	stmt := `SELECT id, title, content, created, expires FROM snippets
+WHERE expires > UTC_TIMESTAMP() AND id = ?`
 
-	return nil, nil
+	row := m.DB.QueryRow(stmt, id)
+
+	s := &models.Snippet{}
+	err := row.Scan(&s.ID, &s.Title, &s.Content, &s.Created, &s.Expires)
+
+	if err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return nil, models.ErrNoRecord
+		} else {
+			return nil, err
+		}
+	}
+	return s, nil
 
 }
 
